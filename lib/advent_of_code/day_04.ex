7d7c1781a6166e61a03b5f6f24e0eeef2479c86b
Caleb OLeary <caleb.oleary@pdq.com>
2023-12-03 23:50:03 -0600

    pt 2 recursion yay

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -46,6 +46,48 @@ defmodule AdventOfCode.Day04 do
 
   end
 
+  defp recurseCountCards(matchCountsPerCard, cardCounts, currIndex) do
+    
+    if (currIndex < length(matchCountsPerCard)) do
+      newCardCounts = List.duplicate(:blah, Enum.at(matchCountsPerCard, currIndex))
+      |> Enum.with_index()
+      |> Enum.reduce(cardCounts, fn {_, index}, acc -> 
+        newCount = Enum.at(cardCounts, currIndex + (index + 1)) + Enum.at(cardCounts, currIndex)
+        List.replace_at(acc,  currIndex + (index + 1), newCount)
+      end)
+      # |> IO.inspect()
+
+      recurseCountCards(matchCountsPerCard, newCardCounts, currIndex + 1)
+    else
+      cardCounts
+    end
+    
+  end
+
   def part2(_args) do
+    input = AdventOfCode.Input.get!(4, 2023)
+#     input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+# Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+# Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+# Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+# Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+# Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+
+    parsed = parseInput(input)
+    
+    matchCountsPerCard = Enum.map(parsed, fn numSet -> 
+      nonIntersection = numSet["myNums"] -- numSet["winningNums"]
+      inersections =  numSet["myNums"] -- nonIntersection
+    end)
+    # |> IO.inspect([charlists: :as_lists])
+    |> Enum.map(fn matches -> 
+      length(matches)
+    end)
+    # |> IO.inspect([charlists: :as_lists])
+
+    recurseCountCards(matchCountsPerCard, List.duplicate(1, length(parsed)), 0)
+    |> Enum.reduce(0, fn currTotal, num -> currTotal + num end)
+
+
   end
 end
