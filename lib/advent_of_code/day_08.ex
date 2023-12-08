984b6623f2422e4b9ebe780cb60d106d2aa1cb68
Caleb OLeary <caleb.oleary@pdq.com>
2023-12-08 00:17:26 -0600

    part 2 was a sort of tricky math thing. leaving all my commented stuff in as a representation of the few ways I tried to tackle this

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -21,6 +21,18 @@ defmodule AdventOfCode.Day08 do
 # GGG = (GGG, GGG)
 # ZZZ = (ZZZ, ZZZ)"
 
+    # # 6
+#     input = "LR
+
+# 11A = (11B, XXX)
+# 11B = (XXX, 11Z)
+# 11Z = (11B, XXX)
+# 22A = (22B, XXX)
+# 22B = (22C, 22C)
+# 22C = (22Z, 22Z)
+# 22Z = (22B, 22B)
+# XXX = (XXX, XXX)"
+
     [directions, locations] = input
     |> String.split("\n\n", trim: true)
 
@@ -45,9 +57,9 @@ defmodule AdventOfCode.Day08 do
 
   end
 
-  defp traverse(directions, locations, step_index, current_location) do
+  defp traverse(directions, locations, step_index, current_location, terminus \\ "ZZZ") do
 
-    if (current_location == "ZZZ") do
+    if (current_location == terminus) do
       step_index
     else 
 
@@ -76,11 +88,138 @@ defmodule AdventOfCode.Day08 do
     input = get_parsed_input()
 
     traverse(input.directions, input.locations, 0, "AAA")
-
-
+    
 
   end
 
+  defp process_parallel_paths(directions, locations, step_index, paths) do
+
+    if (rem(step_index, 1000) == 0) do
+      IO.inspect(step_index)
+      # IO.inspect(Enum.at(paths, 0) |> length())
+      IO.puts("-----")
+    end
+
+    Enum.map(paths, fn path -> 
+      current_location = Enum.at(path, -1)
+
+      wrapped_step_index = rem(step_index, String.length(directions))
+
+      current_direction = String.at(directions, wrapped_step_index)
+
+      index = if (current_direction == "L") do
+        0
+      else
+        1
+      end
+
+      new_path = [locations[current_location] |> Enum.at(index)]
+      
+    end)
+  end
+
+  defp get_do_all_paths_terminate_in_Z(paths) do
+    # IO.inspect(paths)
+    Enum.all?(paths, fn path -> 
+      String.ends_with?(Enum.at(path, -1), "Z")
+    end)
+  end
+
+  defp traverse_pt2(directions, locations, step_index, current_location) do
+
+    if (String.ends_with?(current_location, "Z")) do
+      step_index
+    else 
+
+      wrapped_step_index = rem(step_index, String.length(directions))
+
+      current_direction = String.at(directions, wrapped_step_index)
+
+      index = if (current_direction == "L") do
+        0
+      else
+        1
+      end
+
+      traverse_pt2(
+        directions,
+        locations,
+        step_index + 1,
+        locations[current_location] |> Enum.at(index)
+      )
+    end
+    
+  end
+
+  # defp simulate_n_steps(directions, locations, step_index, path, total_steps) do
+
+  #   IO.inspect(step_index)
+  #   IO.inspect(path)
+
+  #   if (step_index >= total_steps || Enum.at(path, -1) |> String.ends_with?("Z")) do
+  #     IO.inspect(step_index)
+      
+  #     path
+  #   else 
+
+  #     wrapped_step_index = rem(step_index, String.length(directions))
+
+  #     current_location = Enum.at(path, -1)
+
+  #     current_direction = String.at(directions, wrapped_step_index)
+
+  #     index = if (current_direction == "L") do
+  #       0
+  #     else
+  #       1
+  #     end
+
+  #     simulate_n_steps(
+  #       directions,
+  #       locations,
+  #       step_index + 1,
+  #       path ++ [locations[current_location] |> Enum.at(index)],
+  #       total_steps
+  #     )
+  #   end
+    
+  # end
+
+
   def part2(_args) do
+
+    input = get_parsed_input()
+
+    all_locs_ending_in_A = input[:locations]
+    |> Enum.filter(fn {name, _} -> String.ends_with?(name, "A") end)
+    |> Enum.flat_map(fn {name, _} -> [name] end)
+
+    steps_until_z = Enum.map(all_locs_ending_in_A, fn loc -> 
+      traverse_pt2(input[:directions], input[:locations], 0, loc)
+    end)
+
+    #lcm of all numbers
+    Enum.reduce(steps_until_z, 1, fn x, acc ->
+      Math.lcm(x, trunc(acc))
+    end)
+
+    # sim = simulate_n_steps(input[:directions], input[:locations], 0, all_locs_ending_in_A, 1000000)
+
+    # sim2 = simulate_n_steps(input[:directions], input[:locations], 0, [Enum.at(sim, -1)], 2)
+
+
+    # Enum.reduce_while(1..100, 0, fn x, acc ->
+    #   if x < 5, do: {:cont, acc + x}, else: {:halt, acc}
+    # end)
+
+    # path_len = Enum.reduce_while(10000000..100000000, all_locs_ending_in_A, fn curr_depth, paths ->
+      
+    #   if get_do_all_paths_terminate_in_Z(paths) do
+    #     {:halt, curr_depth}
+    #   else
+    #     {:cont, process_parallel_paths(input[:directions], input[:locations], curr_depth, paths)}
+    #   end
+    # end)
+
   end
 end
