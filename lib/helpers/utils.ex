d65be311a3d8897f5505d92c2af12fe6d0db1f61
Caleb OLeary <caleb.oleary@pdq.com>
2023-12-08 19:03:55 -0600

    some bug hunting utils

--- lib/helpers/utils.ex
+++ lib/helpers/utils.ex
@@ -4,4 +4,21 @@ defmodule Helpers.Utils do
         IO.inspect(thing, [charlists: :as_lists])
     end
 
+    def dump_to_file(data, filename) do
+        to_write = data
+        |> Enum.join("\n")
+    
+        File.write!("dumps/#{filename}.txt", to_write)
+    
+        data
+    end
+
+    def log_interval(data_to_log, log_when_divisible_by, step) do
+        if rem(step, log_when_divisible_by) == 0 do
+            inspect(data_to_log)
+        end
+
+        data_to_log
+    end
+
 end
